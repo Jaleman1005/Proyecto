d1e1879996ebd0aa46f58f14a422a5508e311337
Evly <evelyny.guzmanc@konradlorenz.edu.co>
2019-08-17 05:30:05 -0500

    test

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor( public afAuth: AngularFireAuth,private router: Router) { }
+
 
   ngOnInit() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        //this.router.navigateByUrl('/nav');
+        location.href="/nav";
+        
+      }
+    }); 
   }
-
 }
