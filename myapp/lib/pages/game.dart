c71ac09bc2c85188283fd7b38869e839dfaca92a
CDROBAYOM <carlosdrm@gmail.com>
2019-10-01 22:59:31 -0500

    Correct tab routes and name

--- myapp/lib/pages/game.dart
+++ myapp/lib/pages/game.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:draw/pages/quiz.dart';
+import 'package:startup_namer/pages/quiz.dart';
 import 'dart:math';
 
 import '../main.dart';
